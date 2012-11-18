b91bdcd698d7370e6bb61e5f6383d68d27a68170
Daniel Russel <drussel@gmail.com>
2012-11-18 11:07:27 -0800

    make it output-only friendly

--- test/test_frame_comments.py
+++ test/test_frame_comments.py
@@ -5,12 +5,11 @@ import RMF
 class GenericTest(unittest.TestCase):
     def _test_one(self, name):
         f= RMF.create_rmf_file(name)
-        f.set_current_frame(10)
+        f.set_current_frame(0)
         f.set_frame_name("hi")
+        self.assertEqual(f.get_frame_name(), "hi")
         f.set_current_frame(1)
         self.assertEqual(f.get_frame_name(), "")
-        f.set_current_frame(10)
-        self.assertEqual(f.get_frame_name(), "hi")
     def test_open_2x(self):
         """Test frame comments"""
         for suffix in RMF.suffixes:
