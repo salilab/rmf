b427d4bb45970c6b8a373c24eda615785a5c0b10
Daniel Russel <drussel@gmail.com>
2013-10-29 22:09:14 -0700

    clean up spacing

--- src/SetCurrentFrame.cpp
+++ src/SetCurrentFrame.cpp
@@ -8,16 +8,22 @@
 
 #include <RMF/SetCurrentFrame.h>
 
-RMF_ENABLE_WARNINGS RMF_VECTOR_DEF(SetCurrentFrame);
+RMF_ENABLE_WARNINGS
+
+RMF_VECTOR_DEF(SetCurrentFrame);
 
 namespace RMF {
 SetCurrentFrame::SetCurrentFrame(FileConstHandle file, FrameID current_frame)
     : file_(file), old_frame_(file.get_current_frame()) {
-  if (current_frame != FrameID()) {
+  if (current_frame != ALL_FRAMES) {
     file.set_current_frame(current_frame);
   }
 }
-SetCurrentFrame::~SetCurrentFrame() { file_.set_current_frame(old_frame_); }
+SetCurrentFrame::~SetCurrentFrame() {
+  if (old_frame_ != ALL_FRAMES && old_frame_ != FrameID()) {
+      file_.set_current_frame(old_frame_);
+  }
+}
 }
 
 RMF_DISABLE_WARNINGS
