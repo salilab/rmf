5b947518f40e91c81d6baaa4519e0eb76e8d5091
Daniel Russel <drussel@gmail.com>
2013-11-28 10:05:45 -0800

    fix bonds example

--- examples/bonds.py
+++ examples/bonds.py
@@ -9,7 +9,7 @@ def print_bonds(node, bf):
         print "bond", bd.get_bonded()
     else:
         for c in node.get_children():
-            print_bonds(c, bf, obf)
+            print_bonds(c, bf)
 
 fh = RMF.open_rmf_file_read_only(RMF.get_example_path("simple.rmf3"))
 fh.set_current_frame(RMF.FrameID(0))
