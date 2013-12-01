c30c7909b93c054805d41a6a3b5c25099a968460
Daniel Russel <drussel@gmail.com>
2013-12-01 07:43:51 -0800

    remove c++11ism

--- plugins/vmd/rmfplugin.h
+++ plugins/vmd/rmfplugin.h
@@ -152,7 +152,8 @@ std::pair<int, int> Data::get_structure(RMF::NodeConstHandle cur,
         at = tf_.get(cur).get_type_name();
       }
       if (nm.size() >= 16) {
-        std::copy_n(nm.begin(), 15, atoms->name);
+        std::copy(nm.begin(), nm.begin() + std::min(nm.size(), 15),
+                  atoms->name);
         nm[15] = '\0';
       } else {
         std::copy(nm.begin(), nm.end(), atoms->name);
