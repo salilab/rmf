747f9db033ef0eaa1c2fa83de825e8c8fa2ebdaf
Daniel Russel <drussel@gmail.com>
2013-12-03 13:13:29 -0800

    don't use flat set with old VC

--- include/RMF/internal/small_set_map.h
+++ include/RMF/internal/small_set_map.h
@@ -5,14 +5,7 @@
 #include <boost/version.hpp>
 #include <boost/functional/hash/hash.hpp>  // IWYU pragma: export
 
-#if BOOST_VERSION >= 104900
-#include <boost/container/flat_set.hpp>  // IWYU pragma: export
-#include <boost/container/flat_map.hpp>  // IWYU pragma: export
-#define RMF_SMALL_ORDERED_SET boost::container::flat_set
-#define RMF_SMALL_ORDERED_MAP boost::container::flat_map
-#define RMF_SMALL_UNORDERED_SET boost::container::flat_set
-#define RMF_SMALL_UNORDERED_MAP boost::container::flat_map
-#else
+#if BOOST_VERSION < 104900 || defined(_MSC_VER) && _MSC_VER <= 1500
 #include <set>                      // IWYU pragma: export
 #include <map>                      // IWYU pragma: export
 #include <boost/unordered_set.hpp>  // IWYU pragma: export
@@ -22,6 +15,13 @@
 #define RMF_SMALL_ORDERED_MAP std::map
 #define RMF_SMALL_UNORDERED_SET boost::unordered_set
 #define RMF_SMALL_UNORDERED_MAP boost::unordered_map
+#else
+#include <boost/container/flat_set.hpp>  // IWYU pragma: export
+#include <boost/container/flat_map.hpp>  // IWYU pragma: export
+#define RMF_SMALL_ORDERED_SET boost::container::flat_set
+#define RMF_SMALL_ORDERED_MAP boost::container::flat_map
+#define RMF_SMALL_UNORDERED_SET boost::container::flat_set
+#define RMF_SMALL_UNORDERED_MAP boost::container::flat_map
 #endif
 
 #endif
