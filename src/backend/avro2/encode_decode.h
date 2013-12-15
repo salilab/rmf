4d19b3365dd5c5228c17e1cdcd1fbc7fde584b4c
Daniel Russel <drussel@gmail.com>
2013-12-14 17:26:40 -0800

    use skip functions on decode

--- src/backend/avro2/encode_decode.h
+++ src/backend/avro2/encode_decode.h
@@ -41,9 +41,18 @@ namespace internal_avro {
 
 template <class B>
 struct codec_traits<RMF::avro2::Skip<B> > {
+  template <class Decoder>
   static void decode(Decoder& d, RMF::avro2::Skip<B>&) {
     B b;
     internal_avro::decode(d, b);
+  }
+};
+
+template <>
+struct codec_traits<RMF::avro2::Skip<std::string> > {
+  template <class Decoder>
+  static void decode(Decoder& d, RMF::avro2::Skip<std::string>&) {
+    d.skipString();
   }
 };
 
@@ -52,10 +61,12 @@ struct codec_traits<RMF::avro2::Skip<std::vector<B> > >;
 
 template <class K, class V>
 struct codec_traits<std::pair<K, V> > {
+  template <class Encoder>
   static void encode(Encoder& e, const std::pair<K, V>& v) {
     internal_avro::encode(e, v.first);
     internal_avro::encode(e, v.second);
   }
+  template <class Decoder>
   static void decode(Decoder& d, std::pair<K, V>& v) {
     internal_avro::decode(d, v.first);
     internal_avro::decode(d, v.second);
@@ -64,6 +75,7 @@ struct codec_traits<std::pair<K, V> > {
 
 template <class K, class V>
 struct codec_traits<RMF::avro2::Skip<std::pair<K, V> > > {
+  template <class Decoder>
   static void decode(Decoder& d, RMF::avro2::Skip<std::pair<K, V> >&) {
     RMF::avro2::Skip<K> first;
     internal_avro::decode(d, first);
@@ -74,11 +86,15 @@ struct codec_traits<RMF::avro2::Skip<std::pair<K, V> > > {
 
 template <class B>
 struct codec_traits<RMF::avro2::Skip<std::vector<B> > > {
+  template <class Decoder>
   static void decode(Decoder& d, RMF::avro2::Skip<std::vector<B> >&) {
     RMF::avro2::Skip<B> t;
-    for (size_t n = d.arrayStart(); n != 0; n = d.arrayNext()) {
-      for (size_t i = 0; i < n; ++i) {
-        internal_avro::decode(d, t);
+    size_t sa = d.skipArray();
+    if (sa != 0) {
+      for (size_t n = sa; n != 0; n = d.arrayNext()) {
+        for (size_t i = 0; i < n; ++i) {
+          internal_avro::decode(d, t);
+        }
       }
     }
   }
@@ -86,15 +102,17 @@ struct codec_traits<RMF::avro2::Skip<std::vector<B> > > {
 
 template <RMF::VectorDimension V>
 struct codec_traits<RMF_VECTOR<V> > {
+  template <class Encoder>
   static void encode(Encoder& e, const RMF_VECTOR<V>& v) {
     for (unsigned int i = 0; i < V; ++i) {
-      internal_avro::encode<float>(e, v[i]);
+      internal_avro::encode(e, static_cast<float>(v[i]));
     }
   }
+  template <class Decoder>
   static void decode(Decoder& d, RMF_VECTOR<V>& v) {
     for (unsigned int i = 0; i < V; ++i) {
       float f;
-      internal_avro::decode<float>(d, f);
+      internal_avro::decode(d, f);
       v[i] = f;
     }
   }
@@ -102,19 +120,22 @@ struct codec_traits<RMF_VECTOR<V> > {
 
 template <RMF::VectorDimension V>
 struct codec_traits<RMF::avro2::Skip<RMF_VECTOR<V> > > {
+  template <class Decoder>
   static void decode(Decoder& d, RMF::avro2::Skip<RMF_VECTOR<V> >& ) {
     for (unsigned int i = 0; i < V; ++i) {
       float f;
-      internal_avro::decode<float>(d, f);
+      internal_avro::decode(d, f);
     }
   }
 };
 
 template <>
 struct codec_traits<RMF::NodeType> {
+  template <class Encoder>
   static void encode(Encoder& e, const RMF::NodeType& v) {
-    internal_avro::encode<int32_t>(e, v);
+    internal_avro::encode(e, static_cast<int32_t>(v));
   }
+  template <class Decoder>
   static void decode(Decoder& d, RMF::NodeType& v) {
     int32_t vv;
     internal_avro::decode(d, vv);
@@ -124,9 +145,11 @@ struct codec_traits<RMF::NodeType> {
 
 template <>
 struct codec_traits<RMF::FrameType> {
+  template <class Encoder>
   static void encode(Encoder& e, const RMF::FrameType& v) {
-    internal_avro::encode<int32_t>(e, v);
+    internal_avro::encode(e, static_cast<int32_t>(v));
   }
+  template <class Decoder>
   static void decode(Decoder& d, RMF::FrameType& v) {
     int32_t vv;
     internal_avro::decode(d, vv);
@@ -136,10 +159,12 @@ struct codec_traits<RMF::FrameType> {
 
 template <class V>
 struct codec_traits<RMF::ID<V> > {
+  template <class Encoder>
   static void encode(Encoder& e, const RMF::ID<V>& v) {
     RMF_INTERNAL_CHECK(v != RMF::ID<V>(), "Uninitialized ID");
-    internal_avro::encode<int32_t>(e, v.get_index_always());
+    internal_avro::encode(e, static_cast<int32_t>(v.get_index_always()));
   }
+  template <class Decoder>
   static void decode(Decoder& d, RMF::ID<V>& v) {
     int32_t index;
     internal_avro::decode(d, index);
@@ -155,10 +180,12 @@ struct codec_traits<RMF::ID<V> > {
 template <class K, class V>
 struct codec_traits<boost::unordered_map<K, V> > {
   typedef std::pair<K, V> KP;
+  template <class Encoder>
   static void encode(Encoder& e, const boost::unordered_map<K, V>& v) {
     std::vector<KP> pairs(v.begin(), v.end());
     internal_avro::encode(e, pairs);
   }
+  template <class Decoder>
   static void decode(Decoder& d, boost::unordered_map<K, V>& v) {
     std::vector<KP> values;
     internal_avro::decode(d, values);
@@ -168,6 +195,7 @@ struct codec_traits<boost::unordered_map<K, V> > {
 
 template <class K, class V>
 struct codec_traits<RMF::avro2::Skip<boost::unordered_map<K, V> > > {
+  template <class Decoder>
   static void decode(Decoder& d,
                      RMF::avro2::Skip<boost::unordered_map<K, V> >&) {
     RMF::avro2::Skip<std::vector<std::pair<K, V> > > v;
@@ -177,10 +205,12 @@ struct codec_traits<RMF::avro2::Skip<boost::unordered_map<K, V> > > {
 
 template <class K>
 struct codec_traits<boost::unordered_set<K> > {
+  template <class Encoder>
   static void encode(Encoder& e, const boost::unordered_set<K>& v) {
     std::vector<K> values(v.begin(), v.end());
     internal_avro::encode(e, values);
   }
+  template <class Decoder>
   static void decode(Decoder& d, boost::unordered_set<K>& v) {
     std::vector<K> values;
     internal_avro::decode(d, values);
@@ -190,6 +220,7 @@ struct codec_traits<boost::unordered_set<K> > {
 
 template <class K>
 struct codec_traits<RMF::avro2::Skip<boost::unordered_set<K> > > {
+  template <class Decoder>
   static void decode(Decoder& d,
                      RMF::avro2::Skip<boost::unordered_set<K> >&) {
     RMF::avro2::Skip<std::vector<K> > values;
@@ -201,10 +232,12 @@ struct codec_traits<RMF::avro2::Skip<boost::unordered_set<K> > > {
 template <class K, class V>
 struct codec_traits<boost::container::flat_map<K, V> > {
   typedef std::pair<K, V> KP;
+  template <class Encoder>
   static void encode(Encoder& e, const boost::container::flat_map<K, V>& v) {
     std::vector<KP> pairs(v.begin(), v.end());
     internal_avro::encode(e, pairs);
   }
+  template <class Decoder>
   static void decode(Decoder& d, boost::container::flat_map<K, V>& v) {
     std::vector<KP> values;
     internal_avro::decode(d, values);
@@ -214,6 +247,7 @@ struct codec_traits<boost::container::flat_map<K, V> > {
 
 template <class K, class V>
 struct codec_traits<RMF::avro2::Skip<boost::container::flat_map<K, V> > > {
+  template <class Decoder>
   static void decode(Decoder& d,
                      RMF::avro2::Skip<boost::container::flat_map<K, V> >&) {
     RMF::avro2::Skip<std::vector<std::pair<K, V> > > values;
@@ -223,10 +257,12 @@ struct codec_traits<RMF::avro2::Skip<boost::container::flat_map<K, V> > > {
 
 template <class K>
 struct codec_traits<boost::container::flat_set<K> > {
+  template <class Encoder>
   static void encode(Encoder& e, const boost::container::flat_set<K>& v) {
     std::vector<K> values(v.begin(), v.end());
     internal_avro::encode(e, values);
   }
+  template <class Decoder>
   static void decode(Decoder& d, boost::container::flat_set<K>& v) {
     std::vector<K> values;
     internal_avro::decode(d, values);
@@ -236,6 +272,7 @@ struct codec_traits<boost::container::flat_set<K> > {
 
 template <class K>
 struct codec_traits<RMF::avro2::Skip<boost::container::flat_set<K> > > {
+  template <class Decoder>
   static void decode(Decoder& d,
                      RMF::avro2::Skip<boost::container::flat_set<K> >&) {
     RMF::avro2::Skip<std::vector<K> > values;
@@ -247,10 +284,12 @@ struct codec_traits<RMF::avro2::Skip<boost::container::flat_set<K> > > {
 template <class K, class V>
 struct codec_traits<std::map<K, V> > {
   typedef std::pair<K, V> KP;
+  template <class Encoder>
   static void encode(Encoder& e, const std::map<K, V>& v) {
     std::vector<KP> pairs(v.begin(), v.end());
     internal_avro::encode(e, pairs);
   }
+  template <class Decoder>
   static void decode(Decoder& d, std::map<K, V>& v) {
     std::vector<KP> values;
     internal_avro::decode(d, values);
@@ -260,10 +299,12 @@ struct codec_traits<std::map<K, V> > {
 
 template <class K>
 struct codec_traits<std::set<K> > {
+  template <class Encoder>
   static void encode(Encoder& e, const std::set<K>& v) {
     std::vector<K> values(v.begin(), v.end());
     internal_avro::encode(e, values);
   }
+  template <class Decoder>
   static void decode(Decoder& d, std::set<K>& v) {
     std::vector<K> values;
     internal_avro::decode(d, values);
