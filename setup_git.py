b589b0be3dba623bf1b64e40a58c6eeefabe943a
Daniel Russel <drussel@gmail.com>
2014-01-09 11:16:58 -0800

    fix name of other script

--- setup_git.py
+++ setup_git.py
@@ -3,4 +3,4 @@ import os
 import os.path
 os.system("git submodule update --init --recursive")
 os.system("git submodule update --recursive")
-os.system(os.path.join("tools", "developer_tools", "git", "setup-git.py"))
+os.system(os.path.join("tools", "developer_tools", "git", "setup_git.py"))
