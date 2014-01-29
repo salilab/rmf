11329a8e80ad377da3af9d4cb0934fa170f8c794
Daniel Russel <drussel@gmail.com>
2014-01-29 00:02:26 -0800

    update doc

--- classRMF_1_1decorator_1_1GaussianParticleConst.js
+++ classRMF_1_1decorator_1_1GaussianParticleConst.js
@@ -2,11 +2,11 @@ var classRMF_1_1decorator_1_1GaussianParticleConst =
 [
     [ "get_decorator_type_name", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a3f0d837fed370176920d10d04e190d28", null ],
     [ "get_frame_mass", "classRMF_1_1decorator_1_1GaussianParticleConst.html#aa3b58587aba4565a217b00e21dd4e71f", null ],
-    [ "get_frame_standard_deviations", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a275fdefa388b8271d812fc560a95a1fb", null ],
+    [ "get_frame_variances", "classRMF_1_1decorator_1_1GaussianParticleConst.html#adb8002a90978c986fa1ffd836c1daee2", null ],
     [ "get_mass", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a327d9980675aae4da171d7f68e58ee67", null ],
-    [ "get_standard_deviations", "classRMF_1_1decorator_1_1GaussianParticleConst.html#afd14094aa2465a6c150be7c3f7a3a0e4", null ],
     [ "get_static_mass", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a4790941d9fcba0fa8dcc15de4901bddf", null ],
-    [ "get_static_standard_deviations", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a6cd1c7db2265867af16bb43151b631f4", null ],
+    [ "get_static_variances", "classRMF_1_1decorator_1_1GaussianParticleConst.html#aa9763f97edb067fc9f1ad4170a70aaba", null ],
+    [ "get_variances", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a6e24d92b88d1bd2be4479b3ed30ad117", null ],
     [ "GaussianParticle", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a34f1a08c43daff7d663e530b67f2774d", null ],
     [ "GaussianParticleFactory", "classRMF_1_1decorator_1_1GaussianParticleConst.html#a41ca42f068912f31bde703ce29aac888", null ]
 ];
