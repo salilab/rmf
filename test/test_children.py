0b04dc222302672c3a383ac6e1da48f5654ca3e4
Daniel Russel <drussel@gmail.com>
2014-02-19 09:36:20 -0800

    add test that children have right type

---
+++ test/test_children.py
@@ -0,0 +1,11 @@
+import RMF
+
+name = RMF._get_temporary_file_path("children.rmf")
+f = RMF.create_rmf_file(name)
+
+ch = f.get_root_node().add_child("hi", RMF.REPRESENTATION)
+
+chb = f.get_root_node().get_children()[0]
+print chb
+print chb.get_name()
+assert(isinstance(chb, type(ch)))
