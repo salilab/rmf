a34b75883d562c4103cb680d1699c6bec5dc9844
Ben Webb <ben@salilab.org>
2016-10-04 13:28:58 -0700

    Use only non-deprecated code in signature test.

--- test/test_signature.py
+++ test/test_signature.py
@@ -8,7 +8,7 @@ class Tests(unittest.TestCase):
     def test_signature(self):
         """Test get_signature_string()"""
         RMF.set_log_level("Off")
-        path = RMF._get_test_input_file_path("simple.rmf")
+        path = RMF._get_test_input_file_path("simple-new.rmf")
         f = RMF.open_rmf_file_read_only(path)
         sig = RMF.get_signature_string(f)
         with open("simple.rmf.signature", "w") as fh:
