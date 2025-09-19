9615313326b6ae684f9eeb1bf74900f9d1fefb1d
Ben Webb <ben@salilab.org>
2025-09-19 11:49:08 -0700

    Work around test_errors.py failure in parallel builds

--- test/test_errors.py
+++ test/test_errors.py
@@ -14,10 +14,15 @@ class GenericTest(unittest.TestCase):
             import RMF_HDF5
         except ImportError:
             self.skipTest("no support for legacy HDF5 files")
+        # Don't use backwards.rmf, but a copy of it, since if we run tests
+        # in parallel backwards.rmf might be open at the same time by
+        # medium_test_backwards.py, and HDF5 will report an error if we
+        # try to have the same file open RO and RW at the same time.
         f0 = RMF.HDF5.open_file(
-            RMF._get_test_input_file_path("backwards.rmf"))
+            RMF._get_test_input_file_path("backwards_copy.rmf"))
         RMF.HDF5.set_show_errors(True)
-        f1 = RMF.HDF5.open_file(RMF._get_test_input_file_path("backwards.rmf"))
+        f1 = RMF.HDF5.open_file(
+            RMF._get_test_input_file_path("backwards_copy.rmf"))
 
 if __name__ == '__main__':
     unittest.main()
